88d456f3a54044edbcd7b900ebd621f690a5d6ff
maksim5825 <maks.zaletov.99@mail.ru>
2025-01-10 21:00:52 +1000

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,15 @@
+username="имя пользователя"
+title="заголовок заметки"
+content="описание заметки"
+status="статус заметки"
+
+created_date="10.01.2025" # дата создания заметки
+issue_date="10.01.2025"  # дата истечения заметки
+
+print(username,"username", sep="-")
+print(title,"title", sep="-")
+print(content,"content", sep="-")
+print(status,"status", sep="-")
+
+print(created_date [0:5],"created_date", sep="-")
+print(issue_date [0:5],"issue_date", sep="-")
