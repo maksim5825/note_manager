1dc4bcb588ab40b873374aa2f449912fc5357fad
maksim5825 <maks.zaletov.99@mail.ru>
2025-01-21 23:43:59 +1000

    Create dz(1-2).py

---
+++ домашка/dz(1-2).py
@@ -0,0 +1,63 @@
+                                      # задание 1
+# a = int(input("введите число=a:"))
+# b = int(input("введите число=b:"))
+# c = int(input("введите число=c:"))
+# sum = a + b + c
+# multiplication = a*b*c
+# print("сумма:",sum,"произведение:",multiplication )
+# print(f"сумма:{sum} произведение:{multiplication}")
+# print("сумма:{} произведение:{}".format(sum,multiplication))
+                                        # задание 2
+# salary = float(input("зарплата за месяц:"))
+# kredit = float(input("сумма месячного платежа по кредиту в банке:"))
+# debt = float(input("задолженность за коммунальные услуги:"))
+# remainder = salary - kredit - debt
+# print("остаток на конец месяца:",remainder)
+# print(f"остаток на конец месяца:{remainder}")
+# print("остаток на конец месяца:{}".format(remainder))
+                                        # задание 3
+# d1 = int(input("введите диагональ 1:"))
+# d2 = int(input("введите диагональ 2:"))
+# S = d1*d2/2
+# print("площадь ромба равна", S)
+# print(f"площадь ромба равна {S}")
+# print("площадь ромба равна{}".format(S))
+                                        # задание 4
+# print("To be \nor not \nto be")
+                                        # задание 5
+# print("Life is what happens \n\twhen \n\t\tyou're busy making other plans")
+                                        # задание 6
+# m = int(input("введите количество метров :"))
+# dm = m/10
+# sm = m/100
+# mm = m/1000
+# mily = m/1609.34
+# print(f"в {m}метре(ах):\n{dm} дециметров \n{sm} сантиметров \n{mm}милиметров \n{mily}миль")
+                                        # задание 7
+# a = int(input("введите число=a:"))
+# b = int(input("введите число=b:"))
+# delen = a/b
+# print(round(delen,2))
+# print("%.2f"%delen)
+# print("{:.2f}".format(delen))
+                                        # задание 8
+# a = int(input("введите число=a:"))
+# b = int(input("введите число=b:"))
+# a , b = b , a
+# print(a , b)
+                                        # задание 9
+# a = int(input("введите число=a:"))
+# b = int(input("введите число=b:"))
+# c = int(input("введите число=c:"))
+# m =  a , b, c = c , a , b
+# n = a , b , c = b , c , a
+# print(m)
+# print(n)
+                                        # задание 10
+a = int(input("введите сторону квадрата:"))
+r = int(input("введите радиус:"))
+P = a*4
+D = r*2
+print("периметр:",P,"радиус:",D )
+print(f"периметр:{P} радиус:{D}")
+print("периметр:{} радиус:{}".format(P,D))
