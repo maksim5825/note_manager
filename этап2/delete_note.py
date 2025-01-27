85e758cfb3427f747c131aa30b15c7a3159c0ed9
maksim5825 <maks.zaletov.99@mail.ru>
2025-01-28 00:40:20 +1000

    Add files via upload

---
+++ этап2/delete_note.py
@@ -0,0 +1,56 @@
+# создаём список , где будут храниться все заметки
+list_note = []
+def note():  # функция где создаем заметки
+    from datetime import datetime
+    username = input("Введите ваше имя:")  # имя пользователя
+    content = input("Введите описание заметки:")  # описание заметки
+    status = input("Введите статус заметки:")  # статус заметки
+    title = input("Введите заголовок заметки:")  # заголовок заметки
+    current_data = datetime.now().date()
+    current_data_true = current_data.strftime("%d-%m-%Y")
+    issue_date = input("Введите дату дедлайна (в формате день-месяц-год): \n")  # дата истечения заметки
+    dedline = datetime.strptime(issue_date, "%d-%m-%Y").date()
+    deadline_true = dedline.strftime("%d-%m-%Y")
+    note = {  # создаем словарь для заметки
+        'имя пользователя:': username,
+        'заголовок заметки:': title,
+        'описание заметки:': content,
+        'статус заметки:': status,
+        'дата создания заметки:': current_data_true,
+        'дедлайн:': deadline_true
+    }
+    list_note.append(note)  # добавляем заметку в список
+def print_note():  # функция для вывода всех заметок
+    if not list_note:
+        print("Заметок нет)")
+        return
+    for i, note in enumerate(list_note, 1):
+        print(f"\n{i}. имя пользователя: {note['имя пользователя:']}")
+        print(f"   заголовок заметки: {note['заголовок заметки:']}")
+        print(f"   описание заметки: {note['описание заметки:']}")
+        print(f"   статус заметки: {note['статус заметки:']}")
+        print(f"   Дата создания: {note['дата создания заметки:']}")
+        print(f"   Дедлайн: {note['дедлайн:']}")
+def delete_note_username(zaproc2):
+    global list_note
+    for i in range(len(list_note)):
+        if 'имя пользователя:' in list_note[i] or 'заголовок заметки:' in list_note[i]:
+            del list_note[i]
+            print("заметка успешно удалена. Оставшиеся заметки:")
+        else:
+            print("заметка не найдена")
+            break
+def program():
+    print("Добро пожаловать в Менеджер заметок! Вы можете добавить новую заметку.")
+    while 1 > 0:
+        note()  # создаем заметку
+        zaproc = input("\nХотите добавить ещё одну заметку? (да/нет): ")
+        if zaproc != "да":
+            break
+    print("Текущие заметки:", sep="\n" )
+    print_note()
+    zaproc2 = input("Введите имя пользователя или заголовок для удаления заметки:")
+    delete_note_username(zaproc2)
+    print_note()
+program()
+
